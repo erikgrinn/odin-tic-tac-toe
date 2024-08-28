15bcc70fff1be677502be7712d75bebc1141d52b
Erik Grinn <grinn.erik@gmail.com>
2024-08-27 21:30:12 -0400

    logical operators

--- main.js
+++ main.js
@@ -19,27 +19,36 @@ const player1 = new Player(prompt('Enter Player 1 name'), 'X')
 const player2 = new Player(prompt('Enter Player 2 name'), '0')
 player1.turn = true
 
-
-
 let userInput;
 function game(player1, player2) {
 
-    let col1 = gameboard.A[0] + gameboard.B[0] + gameboard.C[0] === 'XXX' || 'OOO'
-    let col2 = gameboard.A[1] + gameboard.B[1] + gameboard.C[1] === 'XXX' || 'OOO'
-    let col3 = gameboard.A[2] + gameboard.B[2] + gameboard.C[2] === 'XXX' || 'OOO'
-    let row1 = gameboard.A[0] + gameboard.A[1] + gameboard.A[2] === 'XXX' || 'OOO'
-    let row2 = gameboard.B[0] + gameboard.B[1] + gameboard.B[2] === 'XXX' || 'OOO'
-    let row3 = gameboard.C[0] + gameboard.C[1] + gameboard.C[2] === 'XXX' || 'OOO'
-    let dia1 = gameboard.A[0] + gameboard.B[1] + gameboard.C[2] === 'XXX' || 'OOO'
-    let dia2 = gameboard.C[0] + gameboard.B[1] + gameboard.A[2] === 'XXX' || 'OOO'
+    let col1 = gameboard.A[0] + gameboard.B[0] + gameboard.C[0]  
+    let col2 = gameboard.A[1] + gameboard.B[1] + gameboard.C[1]
+    let col3 = gameboard.A[2] + gameboard.B[2] + gameboard.C[2]
+    let row1 = gameboard.A[0] + gameboard.A[1] + gameboard.A[2]
+    let row2 = gameboard.B[0] + gameboard.B[1] + gameboard.B[2]
+    let row3 = gameboard.C[0] + gameboard.C[1] + gameboard.C[2]
+    let dia1 = gameboard.A[0] + gameboard.B[1] + gameboard.C[2]
+    let dia2 = gameboard.C[0] + gameboard.B[1] + gameboard.A[2]
 
-    if (col1 || col2 || col3 || row1 || row2 || row3 || dia1 || dia2) {
-        if (col1.charAt(0) == player1.marker) {
-            return (`${player1} wins!`)
-        } else {
-            return (`${player2} wins!`)
-        }
-    }
+    let gameState = 
+    col1 === 'XXX' || col1 === 'OOO' ||
+    col2 === 'XXX' || col2 === 'OOO' ||
+    col3 === 'XXX' || col3 === 'OOO' ||
+    row1 === 'XXX' || row1 === 'OOO' ||
+    row2 === 'XXX' || row2 === 'OOO' ||
+    row3 === 'XXX' || row3 === 'OOO' ||
+    dia1 === 'XXX' || dia1 === 'OOO' ||
+    dia2 === 'XXX' || dia2 === 'OOO';
+
+    console.log(gameState)
+    // if (col1 || col2 || col3 || row1 || row2 || row3 || dia1 || dia2) {
+    //     if (col1.charAt(0) == player1.marker) {
+    //         return (`${player1} wins!`)
+    //     } else {
+    //         return (`${player2} wins!`)
+    //     }
+    // }
 
      if (player1.turn) {
         userInput = prompt(`${player1}: Choose your position (ex. B2)`)
@@ -48,6 +57,7 @@ function game(player1, player2) {
             console.log(gameboard)
             player1.turn = false
             player2.turn = true
+            // game(player1, player2)
          }
      } else if (player2.turn) {
         userInput = prompt(`${player2}: Choose your position (ex. B2)`)
@@ -56,6 +66,7 @@ function game(player1, player2) {
             console.log(gameboard)
             player2.turn = false
             player1.turn = true
+            // game(player1, player2)
          }
      }
 }
