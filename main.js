8815199da6ed50ca105f4f11ae9fbfd75d6a9863
Erik Grinn <grinn.erik@gmail.com>
2024-08-28 12:28:31 -0400

    visual

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const gameboard = {
     A: [null, null, null],
     B: [null, null, null],
     C: [null, null, null],
-}
+};
 
 function updateDisplay(player, row, col) {
-    document.getElementById(row+(col+1)).innerHTML = player.marker
+    document.getElementById(row + (col)).innerHTML = player.marker;
 }
 
 function Player(name, marker) {
-    this.name = name
-    this.marker = marker
-    this.turn = false
+    this.name = name;
+    this.marker = marker;
+    this.turn = false;
 }
 
 function createPlayer(promptMessage, marker) {
@@ -22,82 +22,56 @@ function createPlayer(promptMessage, marker) {
 
 const player1 = createPlayer('Enter Player 1 name', 'X');
 const player2 = createPlayer('Enter Player 2 name', 'O');
-player1.turn = true
+player1.turn = true;
 
-let count = 0
+const gridItems = document.querySelectorAll('.cell');
 
-function game(player1, player2) {
-    let userInput;
+let count = 0;
 
-    if (player1.turn) {
-        userInput = prompt(`${player1.name}: Choose your position (ex. B2)`);
-    } else if (player2.turn) {
-        userInput = prompt(`${player2.name}: Choose your position (ex. B2)`);
-    }
+gridItems.forEach(item => {
+    item.addEventListener('click', () => handleClick(item));
+});
 
-    // Input validation
-    if (userInput.length !== 2 ||
-        !['A', 'B', 'C'].includes(userInput.charAt(0).toUpperCase()) ||
-        isNaN(userInput.charAt(1)) ||
-        parseInt(userInput.charAt(1)) < 1 ||
-        parseInt(userInput.charAt(1)) > 3) {
-        alert('Input not valid');
-        return game(player1, player2)
-    }
+function handleClick(item) {
+    const currentPlayer = player1.turn ? player1 : player2;
 
-    const row = userInput.charAt(0).toUpperCase();
-    const col = parseInt(userInput.charAt(1)) - 1;
+    if (item.innerHTML === '') {
 
-    if (player1.turn) {
-        if (gameboard[row][col] === null) {
-            gameboard[row][col] = player1.marker;
-            updateDisplay(player1, row, col)
-        } else {
-            alert('Cell already occupied');
-            return game(player1, player2);
-        }
-    } else if (player2.turn) {
-        if (gameboard[row][col] === null) {
-            gameboard[row][col] = player2.marker;
-            updateDisplay(player2, row, col)
-        } else {
-            alert('Cell already occupied');
-            return game(player1, player2);
-        }
-    }
+        let row = item.id.charAt(0);
+        let col = parseInt(item.id.charAt(1));
+        updateDisplay(currentPlayer, row, col);
+        gameboard[row][col - 1] = currentPlayer.marker;
 
-    if (winState(gameboard)) {
-        if (player1.turn) {
-            return `${player1.name} wins!`;
-        } else if (player2.turn) {
-            return `${player2.name} wins!`;
-        } 
-    } else if (count === 8) {
-        return "It's a tie!"
+        // Small delay to allow the UI to update before checking win state
+        setTimeout(() => {
+            if (winState()) {
+                alert(`${currentPlayer.name} wins!`);
+                resetGame();
+            } else if (count === 8) {
+                alert("It's a tie!");
+                resetGame();
+            } else {
+                count++;
+                player1.turn = !player1.turn;
+                player2.turn = !player2.turn;
+            }
+        }, 50); // Delay of 50ms
     } else {
-        count++
-        if (player1.turn) {
-            player1.turn = false;
-            player2.turn = true;
-        } else if (player2.turn) {
-            player2.turn = false;
-            player1.turn = true;
-        }
-        return game(player1, player2); // recursive call, needed to display win message
+        alert('The cell is already taken!');
     }
 }
 
 function winState() {
-    let col1 = gameboard.A[0] + gameboard.B[0] + gameboard.C[0]  
-    let col2 = gameboard.A[1] + gameboard.B[1] + gameboard.C[1]
-    let col3 = gameboard.A[2] + gameboard.B[2] + gameboard.C[2]
-    let row1 = gameboard.A[0] + gameboard.A[1] + gameboard.A[2]
-    let row2 = gameboard.B[0] + gameboard.B[1] + gameboard.B[2]
-    let row3 = gameboard.C[0] + gameboard.C[1] + gameboard.C[2]
-    let dia1 = gameboard.A[0] + gameboard.B[1] + gameboard.C[2]
-    let dia2 = gameboard.C[0] + gameboard.B[1] + gameboard.A[2]
+    let col1 = gameboard.A[0] + gameboard.B[0] + gameboard.C[0];
+    let col2 = gameboard.A[1] + gameboard.B[1] + gameboard.C[1];
+    let col3 = gameboard.A[2] + gameboard.B[2] + gameboard.C[2];
+    let row1 = gameboard.A[0] + gameboard.A[1] + gameboard.A[2];
+    let row2 = gameboard.B[0] + gameboard.B[1] + gameboard.B[2];
+    let row3 = gameboard.C[0] + gameboard.C[1] + gameboard.C[2];
+    let dia1 = gameboard.A[0] + gameboard.B[1] + gameboard.C[2];
+    let dia2 = gameboard.C[0] + gameboard.B[1] + gameboard.A[2];
 
-    let gameState =
+    return (
         col1 === 'XXX' || col1 === 'OOO' ||
         col2 === 'XXX' || col2 === 'OOO' ||
         col3 === 'XXX' || col3 === 'OOO' ||
@@ -105,9 +79,21 @@ function winState() {
         row2 === 'XXX' || row2 === 'OOO' ||
         row3 === 'XXX' || row3 === 'OOO' ||
         dia1 === 'XXX' || dia1 === 'OOO' ||
-        dia2 === 'XXX' || dia2 === 'OOO';
-
-    return gameState
+        dia2 === 'XXX' || dia2 === 'OOO'
+    );
 }
 
-console.log(game(player1, player2))
+function resetGame() {
+    count = 0;
+    player1.turn = true;
+    player2.turn = false;
+    for (let row in gameboard) {
+        gameboard[row] = [null, null, null];
+    }
+    gridItems.forEach(item => {
+        item.innerHTML = '';
+    });
+}
+
+alert(`${player1.name}'s turn. Pick a tile`);
+
